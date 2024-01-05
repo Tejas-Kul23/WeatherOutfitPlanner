841bb69023d8c05753efeee355f005de96d5bf30
Tejas Kulthe <tejas.kulthe23@gmail.com>
2024-01-05 22:25:52 +0000

    Implement LoginUser functionality

--- weather_outfit_planner.py
+++ weather_outfit_planner.py
@@ -1,26 +1,27 @@
-#Import Required Libraries
+# Import Required Libraries
 import tkinter as tk
 from tkinter import ttk, W
 import hashlib
 
-#Initialise Global Variables:
+# Initialise Global Variables:
 username = ''
 
-#Define Base Class - for Other Frames and Class
+
+# Define Base Class - for Other Frames and Class
 class App(tk.Tk):
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
 
-        #The Container is where other frames will be stacked upon
-        #The ones that need to be made visible, will be raised above others
+        # The Container is where other frames will be stacked upon
+        # The ones that need to be made visible, will be raised above others
         container = tk.Frame(self)
 
-        #Ensures the frame will fill the entire tkinter app regardless of its size
+        # Ensures the frame will fill the entire tkinter app regardless of its size
         container.pack(side='top', fill='both', expand=True)
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
 
-        #Create a dictionary, with keys being the names of each frame class,
+        # Create a dictionary, with keys being the names of each frame class,
         # and the values being the instances of frame classes themselves
         self.frames = {}
 
@@ -31,125 +32,193 @@ class App(tk.Tk):
             self.frames[page_name] = frame
             frame.grid(row=0, column=0, sticky='nsew')
 
-
     def show_frame(self, page_name: str) -> None:
-        #Gets the Frame Instance by reffering to the Key Value
+        # Gets the Frame Instance by reffering to the Key Value
         frame = self.frames[page_name]
         # raises the current frame to the top
         frame.tkraise()
 
+
 class RegisterUser(tk.Frame):
-    def __init__(self, parent,controller) -> None:
+    def __init__(self, parent, controller) -> None:
         tk.Frame.__init__(self, parent)
         self.controller = controller
 
-        #Sets the title of the Application
+        # Sets the title of the Application
         self.controller.title('Register User')
 
-        #Initialise variables that will be used to get values from corresponding entry boxes
+        # Initialise variables that will be used to get values from corresponding entry boxes
         self.user_var = tk.StringVar()
         self.pwd_var = tk.StringVar()
         self.conf_pwd_var = tk.StringVar()
 
-
-        #Instantiate Labels and Entry Boxes for Username, Password and Confirmed Password
-        #Each Label is aligned to the left
-        #Padding of 10px is applied on the left and right
+        # Instantiate Labels and Entry Boxes for Username, Password and Confirmed Password
+        # Each Label is aligned to the left
+        # Padding of 10px is applied on the left and right
 
         self.user_label = ttk.Label(self, text='Username: ', anchor='w', justify='left')
-        self.user_label.grid(row=0, column=0, sticky = W, padx= 10)
+        self.user_label.grid(row=0, column=0, sticky=W, padx=10)
         self.user_entry = ttk.Entry(self, width=50, textvariable=self.user_var)
-        self.user_entry.grid(row=1, column=0, padx= 10)
+        self.user_entry.grid(row=1, column=0, padx=10)
 
-        #Users entry to the password is censored by the use of asterisks '*'
+        # Users entry to the password is censored by the use of asterisks '*'
 
         self.pwd_label = ttk.Label(self, text='Password: ', anchor='w', justify='left')
-        self.pwd_label.grid(row=2, column=0, sticky = W, padx= 10)
+        self.pwd_label.grid(row=2, column=0, sticky=W, padx=10)
         self.pwd_entry = ttk.Entry(self, width=50, textvariable=self.pwd_var, show='*')
-        self.pwd_entry.grid(row=3, column=0, padx= 10)
+        self.pwd_entry.grid(row=3, column=0, padx=10)
 
         self.conf_pwd_label = ttk.Label(self, text='Confirm Password: ', anchor='w', justify='left')
-        self.conf_pwd_label.grid(row=4, column=0, sticky = W, padx= 10)
+        self.conf_pwd_label.grid(row=4, column=0, sticky=W, padx=10)
         self.conf_pwd_entry = ttk.Entry(self, width=50, show='*', textvariable=self.conf_pwd_var)
-        self.conf_pwd_entry.grid(row=5, column=0, padx= 10)
+        self.conf_pwd_entry.grid(row=5, column=0, padx=10)
 
-        #Button instantiated that will run the method self.register()
-        self.register_response = ttk.Label(self, text = '')
+        # Button instantiated that will run the method self.register()
+        self.register_response = ttk.Label(self, text='')
         self.register_response.grid(row=6, column=0, padx=10)
 
-        self.register_btn = ttk.Button(self, text='Register', command=self.register)
+        self.register_btn = ttk.Button(self, text='Register', command=self.register_user)
         self.register_btn.grid(row=7, column=0, padx=10)
 
-
-    def register(self) -> None:
-        #Take values from the entry boxes
+    def register_user(self) -> None:
+        # Take values from the entry boxes
         global username
         username = self.user_var.get()
         password = self.pwd_var.get()
         confirm_password = self.conf_pwd_var.get()
 
-        #Presence Check
+        # Presence Check
         if username != '' and password != '' and confirm_password != '':
-            #Check whether the Password is re-entered correctly
+            # Check whether the Password is re-entered correctly
             if password == confirm_password:
 
-                #Update register label text to confirm success
+                # Update register label text to confirm success
                 self.register_response.config(text='Registered!')
                 self.user_var.set('')
+                self.pwd_var.set('')
+                self.conf_pwd_var.set('')
 
-                #password string is encoded in order to be hashed, then represented in hexadecimal
-                #and saved in a password.txt file
+                # password string is encoded in order to be hashed, then represented in hexadecimal
+                # and saved in a password.txt file
                 conf_pwd_hash = hashlib.sha256(confirm_password.encode()).hexdigest()
 
-                #Consider using csv file if possible???
+                # Consider using csv file if possible???
                 with open('password.txt', 'w', encoding='utf-8') as f:
                     f.write(conf_pwd_hash)
 
                 confirm_password, password = '', ''
-            else:
-                #Update register label text to inform a failed match
-                self.register_response.config(text='Confirmed Password does not match the Password')
 
-            self.pwd_var.set('')
-            self.conf_pwd_var.set('')
+                self.controller.show_frame("LoginUser")
+            else:
+                # Update register label text to inform a failed match
+                self.register_response.config(text='Confirmed Password does not match the Password')
+                self.pwd_var.set('')
+                self.conf_pwd_var.set('')
+
+
         else:
-            #Update register label text to inform a failed presence check
+            # Update register label text to inform a failed presence check
             self.register_response.config(text='Please do not leave any fields empty')
 
 
 class LoginUser(tk.Frame):
-    def __init__(self, parent,controller) -> None:
+    def __init__(self, parent, controller) -> None:
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        self.controller.title('Login User')
+
+        self.user_var = tk.StringVar()
+        self.pwd_var = tk.StringVar()
+
+        # Create Entry Boxes and Labels for Login Page
+        self.user_label = ttk.Label(self, text='Username: ', anchor='w', justify='left')
+        self.user_label.grid(row=0, column=0, sticky=W, padx=10)
+        self.user_entry = ttk.Entry(self, width=50, textvariable=self.user_var)
+        self.user_entry.grid(row=1, column=0, padx=10)
+
+        # Users entry to the password is censored by the use of asterisks '*'
+
+        self.pwd_label = ttk.Label(self, text='Password: ', anchor='w', justify='left')
+        self.pwd_label.grid(row=2, column=0, sticky=W, padx=10)
+        self.pwd_entry = ttk.Entry(self, width=50, textvariable=self.pwd_var, show='*')
+        self.pwd_entry.grid(row=3, column=0, padx=10)
+
+        # Implement Login Button to run method login()
+        self.login_response = ttk.Label(self, text='')
+        self.login_response.grid(row=4, column=0, padx=10)
+
+        self.login_btn = ttk.Button(self, text='Login', command=self.login)
+        self.login_btn.grid(row=5, column=0, padx=10)
+
+        # Future Potential Additions, Change password, or username
+
+    def login(self) -> None:
+        login_user_var = self.user_var.get()
+        login_pwd_var = self.pwd_entry.get()
+        # Presence Check
+        if login_user_var == '':
+            self.login_response.config(text='Username entry field must be filled')
+            return
+        if login_pwd_var == '':
+            self.login_response.config(text='Password entry field must be filled')
+            return
+
+        # The string entered in pwd_entry is accessed, encoded, hashed and converted to a hexadecimal
+        pwd_hash = hashlib.sha256(login_pwd_var.encode()).hexdigest()
+
+        # text file is open, and the hash is read and saved to variable
+        pwd_txt_hash = open('password.txt', encoding='utf-8').read()
+
+        # Username and Variable Compared
+        if login_user_var == username and pwd_hash == pwd_txt_hash:
+            self.login_response.config(text='Logged in successfully!')
+            self.controller.show_frame('Homepage')
+        else:
+            self.login_response.config(text='Incorrect password/username, Try again.')
+        return
+
+
+class Homepage(tk.Frame):
+    def __init__(self, parent, controller) -> None:
         tk.Frame.__init__(self, parent)
         self.controller = controller
 
-class Homepage(tk.Frame):
-    def __init__(self, parent,controller) -> None:
-        tk.Frame.__init__(self, parent)
-        self.controller = controller
+
 class AddClothes(tk.Frame):
-    def __init__(self, parent,controller) -> None:
+    def __init__(self, parent, controller) -> None:
         tk.Frame.__init__(self, parent)
         self.controller = controller
+
+
 class EditClothes(tk.Frame):
-    def __init__(self, parent,controller) -> None:
+    def __init__(self, parent, controller) -> None:
         tk.Frame.__init__(self, parent)
         self.controller = controller
+
+
 class EditListClothes(tk.Frame):
-    def __init__(self, parent,controller) -> None:
+    def __init__(self, parent, controller) -> None:
         tk.Frame.__init__(self, parent)
         self.controller = controller
+
+
 class EditClothesPages(tk.Frame):
-    def __init__(self, parent,controller) -> None:
+    def __init__(self, parent, controller) -> None:
         tk.Frame.__init__(self, parent)
         self.controller = controller
+
+
 class DeleteClothes(tk.Frame):
-    def __init__(self, parent,controller) -> None:
+    def __init__(self, parent, controller) -> None:
         tk.Frame.__init__(self, parent)
         self.controller = controller
+
+
 class DeleteListClothes(tk.Frame):
-    def __init__(self, parent,controller) -> None:
+    def __init__(self, parent, controller) -> None:
         tk.Frame.__init__(self, parent)
         self.controller = controller
+
 
 if __name__ == '__main__':
     app = App()
