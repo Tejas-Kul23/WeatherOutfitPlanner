3f6bdd7ef2441d152ac7a59e4dd7e10740011222
Tejas Kulthe <tejas.kulthe23@gmail.com>
2024-01-07 10:28:34 +0000

    Organise code to improve readability

--- weather_outfit_planner.py
+++ weather_outfit_planner.py
@@ -1,6 +1,6 @@
 # Import Required Libraries
 import tkinter as tk
-from tkinter import ttk, W
+from tkinter import ttk
 import hashlib
 
 # Initialise Global Variables:
@@ -33,7 +33,7 @@ class App(tk.Tk):
             frame.grid(row=0, column=0, sticky='nsew')
 
     def show_frame(self, page_name: str) -> None:
-        # Gets the Frame Instance by reffering to the Key Value
+        # Gets the Frame Instance by referring to the Key Value
         frame = self.frames[page_name]
         # raises the current frame to the top
         frame.tkraise()
@@ -44,9 +44,6 @@ class RegisterUser(tk.Frame):
         tk.Frame.__init__(self, parent)
         self.controller = controller
 
-        # Sets the title of the Application
-        self.controller.title('Register User')
-
         # Initialise variables that will be used to get values from corresponding entry boxes
         self.user_var = tk.StringVar()
         self.pwd_var = tk.StringVar()
@@ -54,31 +51,29 @@ class RegisterUser(tk.Frame):
 
         # Instantiate Labels and Entry Boxes for Username, Password and Confirmed Password
         # Each Label is aligned to the left
-        # Padding of 10px is applied on the left and right
-
         self.user_label = ttk.Label(self, text='Username: ', anchor='w', justify='left')
-        self.user_label.grid(row=0, column=0, sticky=W, padx=10)
         self.user_entry = ttk.Entry(self, width=50, textvariable=self.user_var)
-        self.user_entry.grid(row=1, column=0, padx=10)
 
         # Users entry to the password is censored by the use of asterisks '*'
-
         self.pwd_label = ttk.Label(self, text='Password: ', anchor='w', justify='left')
-        self.pwd_label.grid(row=2, column=0, sticky=W, padx=10)
         self.pwd_entry = ttk.Entry(self, width=50, textvariable=self.pwd_var, show='*')
-        self.pwd_entry.grid(row=3, column=0, padx=10)
-
         self.conf_pwd_label = ttk.Label(self, text='Confirm Password: ', anchor='w', justify='left')
-        self.conf_pwd_label.grid(row=4, column=0, sticky=W, padx=10)
         self.conf_pwd_entry = ttk.Entry(self, width=50, show='*', textvariable=self.conf_pwd_var)
-        self.conf_pwd_entry.grid(row=5, column=0, padx=10)
+
+        # Padding of 10px is applied on the x-axis
+        self.user_label.pack(side=tk.TOP, padx=10)
+        self.user_entry.pack(side=tk.TOP, padx=10)
+        self.pwd_label.pack(side=tk.TOP, padx=10)
+        self.pwd_entry.pack(side=tk.TOP, padx=10)
+        self.conf_pwd_label.pack(side=tk.TOP, padx=10)
+        self.conf_pwd_entry.pack(side=tk.TOP, padx=10)
 
         # Button instantiated that will run the method self.register()
         self.register_response = ttk.Label(self, text='')
-        self.register_response.grid(row=6, column=0, padx=10)
-
         self.register_btn = ttk.Button(self, text='Register', command=self.register_user)
-        self.register_btn.grid(row=7, column=0, padx=10)
+
+        self.register_response.pack(side=tk.TOP, padx=10)
+        self.register_btn.pack(side=tk.TOP, padx=10)
 
     def register_user(self) -> None:
         # Take values from the entry boxes
@@ -115,7 +110,6 @@ class RegisterUser(tk.Frame):
                 self.pwd_var.set('')
                 self.conf_pwd_var.set('')
 
-
         else:
             # Update register label text to inform a failed presence check
             self.register_response.config(text='Please do not leave any fields empty')
@@ -125,32 +119,31 @@ class LoginUser(tk.Frame):
     def __init__(self, parent, controller) -> None:
         tk.Frame.__init__(self, parent)
         self.controller = controller
-        self.controller.title('Login User')
-
         self.user_var = tk.StringVar()
         self.pwd_var = tk.StringVar()
 
         # Create Entry Boxes and Labels for Login Page
-        self.user_label = ttk.Label(self, text='Username: ', anchor='w', justify='left')
-        self.user_label.grid(row=0, column=0, sticky=W, padx=10)
+        self.user_label = ttk.Label(self, text='Username: ')
         self.user_entry = ttk.Entry(self, width=50, textvariable=self.user_var)
-        self.user_entry.grid(row=1, column=0, padx=10)
 
         # Users entry to the password is censored by the use of asterisks '*'
-
-        self.pwd_label = ttk.Label(self, text='Password: ', anchor='w', justify='left')
-        self.pwd_label.grid(row=2, column=0, sticky=W, padx=10)
+        self.pwd_label = ttk.Label(self, text='Password: ')
         self.pwd_entry = ttk.Entry(self, width=50, textvariable=self.pwd_var, show='*')
-        self.pwd_entry.grid(row=3, column=0, padx=10)
 
-        # Implement Login Button to run method login()
+        # Response Label Updated to inform any error within the data
         self.login_response = ttk.Label(self, text='')
-        self.login_response.grid(row=4, column=0, padx=10)
-
+        # Runs Login()
         self.login_btn = ttk.Button(self, text='Login', command=self.login)
-        self.login_btn.grid(row=5, column=0, padx=10)
 
-        # Future Potential Additions, Change password, or username
+        # Packing all widgets, padding of 10px on the x-axis
+        self.user_label.pack(side=tk.TOP, padx=10, anchor=tk.W)
+        self.user_entry.pack(side=tk.TOP)
+        self.pwd_label.pack(side=tk.TOP, padx=10, anchor=tk.W)
+        self.pwd_entry.pack(side=tk.TOP)
+        self.login_response.pack(side=tk.TOP)
+        self.login_btn.pack(side=tk.TOP)
+
+    # Future Potential Additions, Change password, or username
 
     def login(self) -> None:
         login_user_var = self.user_var.get()
@@ -222,5 +215,6 @@ class DeleteListClothes(tk.Frame):
 
 if __name__ == '__main__':
     app = App()
+    app.title('Weather Outfit Planner')
     app.show_frame('RegisterUser')
     app.mainloop()
